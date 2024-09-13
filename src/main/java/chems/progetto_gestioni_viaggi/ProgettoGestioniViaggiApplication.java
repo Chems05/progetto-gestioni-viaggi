ac089ebb00dfda8f068b84bcfb94d26637c6ff75
Sun/chems Zghaidi <154335653+Chems05@users.noreply.github.com>
2024-09-13 11:52:22 +0200

    Initial commit

---
+++ src/main/java/chems/progetto_gestioni_viaggi/ProgettoGestioniViaggiApplication.java
@@ -0,0 +1,13 @@
+package chems.progetto_gestioni_viaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoGestioniViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoGestioniViaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chems/progetto_gestioni_viaggi/ProgettoGestioniViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package chems.progetto_gestioni_viaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoGestioniViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
